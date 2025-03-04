caa264dbe577f4643a1eb10e4644ab71d1cc6d37
Jaewon <dhwodnjs0827@naver.com>
2025-03-04 19:52:34 +0900

    자원 채취

--- Assets/Scripts/Item/EquipTool.cs
+++ Assets/Scripts/Item/EquipTool.cs
@@ -10,17 +10,19 @@ public class EquipTool : Equip
     public float attackDistance;
     
     [Header("Resource Gathering")]
-    public bool doesGatherResource;
+    public bool doesGatherResources;
 
     [Header("Combat")]
     public bool doesDealDamage;
     public int damage;
     
     private Animator animator;
+    private Camera camera;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        camera = Camera.main;
     }
 
     public override void OnAttackInput()
@@ -36,5 +38,18 @@ public class EquipTool : Equip
     private void OnCanAttack()
     {
         attacking = false;
+    }
+
+    public void OnHit()
+    {
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, attackDistance))
+        {
+            if (doesGatherResources && hit.collider.TryGetComponent(out Resource resource))
+            {
+                resource.Gather(hit.point, hit.normal);
+            }
+        }
     }
 }
